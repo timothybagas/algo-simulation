eb64c040e8cef6b395acf171ec044be383fa10c4
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-04-02 15:29:39 +0700

    Create a content card component

---
+++ components/ContentCard.jsx
@@ -0,0 +1,26 @@
+export default function ContentCard({
+  title,
+  algorithms,
+}) {
+  return (
+    <div className="rounded-xl drop-shadow-xl bg-white hover:bg-gray-100 cursor-pointer">
+      <div className="p-5">
+        <h2 className="font-bold text-2xl">{title}</h2>
+      </div>
+      <div className="bg-black w-full h-36"></div>
+      <div className="p-5 space-y-3">
+        <p className="font-bold">{"List of algorithms :"}</p>
+        <div className="w-full">
+          {algorithms.map((algorithm, key) => (
+            <div
+              className="bg-gray-200 p-2 hover:bg-sky-400 hover:text-white duration-200 ease-in rounded-lg w-fit inline-block m-1"
+              key={key}
+            >
+              {algorithm}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>  
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+import ContentCard from "../components/ContentCard"
 import Layout from "../components/Layout"
 
 export default function Home() {
   return (
     <Layout>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
-        {"Hello World"}
+        <ContentCard
+          title="Grid Algorithms"
+          algorithms={["Bread First Search", "Dijkstra", "A-Star", "Depth First Search"]}
+        />
       </div>
     </Layout>
   )
