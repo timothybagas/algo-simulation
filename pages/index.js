cf20db3bb3d5c8ac5db26e24e5009653f3b31d1c
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-04-02 15:00:08 +0700

    Create initial layout for home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
-      <div className="text-3xl font-bold">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
         {"Hello World"}
       </div>
     </Layout>
