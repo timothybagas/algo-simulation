b12630ee57bc4438680990efb1a1ee45240e65d6
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-04-04 15:21:57 +0700

    Update logo in navigation bar to make it clickable

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,17 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
     <header className="flex justify-between bg-blue-900 py-5 px-10">
-      <div className="text-white font-bold text-xl flex items-center">
-        <div className="bg-sky-400 p-2 rounded-xl">
-          <p>{"algo"}</p>
-        </div>
-        <p className="ml-1">{"simulation"}</p>
+      <div className="text-white font-bold text-xl">
+        <Link href="/">
+          <a className="flex items-center">
+            <div className="bg-sky-400 p-2 rounded-xl">
+              <p>{"algo"}</p>
+            </div>
+            <p className="ml-1">{"simulation"}</p>
+          </a>
+        </Link>
       </div>
     </header>
   )
