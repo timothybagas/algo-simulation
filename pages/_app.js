7365fe1aa965e0c8f5294b2b230b5fb2fcea635b
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-04-05 17:44:44 +0700

    Warn user if accessed the website from mobile device

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { toast, Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (window.innerWidth <= 800) {
+      toast(() => (
+        <p className='ml-1'>
+          {"Please reopen this window on your laptop/desktop if you are currently using a"}
+          <b>{"mobile device"}</b>
+        </p>
+      ), { duration: 6000, });
+    }
+  }, []);
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
+    </>
+  )
 }
 
 export default MyApp
